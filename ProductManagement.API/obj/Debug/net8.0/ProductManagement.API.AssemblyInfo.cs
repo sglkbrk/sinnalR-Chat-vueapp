36bdbfdfb56bc98db1ec6f86cd6b83cc8b1532be
burak <sglk.brk@gmail.com>
2024-10-09 23:14:23 +0300

    mesaj ekranı düzenlemeleri

--- ProductManagement.API/obj/Debug/net8.0/ProductManagement.API.AssemblyInfo.cs
+++ ProductManagement.API/obj/Debug/net8.0/ProductManagement.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductManagement.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94e6008808a27e89fa4e0ce36573e9904a318fca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c65200cb28b476b98a21e975da1c818763deba46")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductManagement.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductManagement.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
